048e11562737c7f058496ee9a1aa5f739b029fb1
Shiven Chhugani <153463067+ShivenC@users.noreply.github.com>
2026-07-21 14:39:23 -0400

    Update generate_tickets.py

--- generate_tickets.py
+++ generate_tickets.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
+
 JIRA_SERVER = os.getenv("JIRA_SERVER")
 JIRA_EMAIL = os.getenv("JIRA_EMAIL")
 JIRA_API_TOKEN = os.getenv("JIRA_API_TOKEN")
